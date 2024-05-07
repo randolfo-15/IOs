aea7a13ac4554ac8d25ccaa598cbee7613d8ef16
Randolfo A Goncalves <ragnifico@yahoo.com.br>
2024-05-07 11:12:12 -0300

    Lib IOs

--- IOs.java
+++ IOs.java
@@ -1,5 +1,16 @@
+/*!**********************************************************  
+ *  Ios 
+ * =====
+ * @author: Randolfo A Goncalves
+ * @date  : 07/05/24
+ * @file  : IOs.java
+*************************************************************/
+
+//! Imports:
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,45 +18,37 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-public class IOs implements Write,Read{
-    @Override
-    public void make(FileReader file, String text) {
-        BufferedReader bf =new BufferedReader(file);
-        String line ="";
-        try{ while(true) 
-                if((line=bf.readLine())==null) break;
-                else text+=line;
-
-             bf.close();
-
-        }catch(IOException e){e.printStackTrace();}
+public class IOs {
+    public static String get_text(String path) throws IOException {
+        BufferedReader bf =new BufferedReader(new FileReader(path));
+        String line ="",text="";
+        while((line=bf.readLine())!=null) text+=line+" \n";
+        bf.close();
+        return text;
     } 
     
-    @Override
-    public void make(FileWriter file, String text) {
-        BufferedWriter bf =new BufferedWriter(file);
-        try{ bf.append(text+" \n");
-             bf.close();
-        
-        }catch(IOException e){ e.printStackTrace(); }
+    public static void set_text(String path, String text)throws IOException {
+        BufferedWriter bf =new BufferedWriter(new FileWriter(path,true));
+        bf.append(text+" \n");
+        bf.close();
     }
     
-    @Override
-    public void make(ObjectInputStream file,Object obj) {
-        try{
-            obj=(Object) file.readObject();
-            file.close();
-        
-        } catch(ClassNotFoundException e){ e.printStackTrace(); 
-        } catch(IOException e){ e.printStackTrace(); }
+    public static void set_text(String path, String text, Boolean append)throws IOException {
+        BufferedWriter bf =new BufferedWriter(new FileWriter(path,append));
+        bf.append(text+" \n");
+        bf.close();
+    }   
+
+    public static Object get_object(String path)throws ClassNotFoundException,IOException {
+        ObjectInputStream file = new ObjectInputStream(new FileInputStream(path));
+        Object obj = file.readObject();
+        file.close();
+        return obj;
     }
 
-    @Override
-    public void make(ObjectOutputStream file, Serializable obj) {
-        try{
-            file.writeObject(obj);
-            file.close(); 
-
-        }catch(IOException e){ e.printStackTrace(); }
+    public static void set_object(String path, Serializable obj)throws IOException {
+        ObjectOutputStream file = new ObjectOutputStream(new FileOutputStream(path));
+        file.writeObject(obj);
+        file.close();
     }
 }
--- Main.java
+++ Main.java
@@ -1,22 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class Letter implements Serializable{
+    public String name;
+    Letter(String name){this.name=name;}
+}
 
 class Main{
     public static void main(String[] args) {
-       IOs io = new IOs();
-       
        try{
-           
-           io.make(new FileWriter("nomes.txt",true), "Randolfo");
-           io.make(new FileWriter("nomes.txt",true), "Pablo");
-           io.make(new FileWriter("nomes.txt",true), "Joao");
-           
-           String nomes="";
-           io.make(new FileReader("nomes.txt"),nomes);
-           System.out.println(nomes);
+           //  Out
+           // =====
+           //IOs.set_text("nomes.txt", "Randolfo"); 
+           //IOs.set_text("nomes.txt", "Pedro"); 
+           //IOs.set_text("nomes.txt", "Carla"); 
+           //IOs.set_object("letter.dat", new Letter("B"));
 
-       }catch(IOException e){}
-
+           //  In
+           // ====
+           System.out.println(
+                   "Nomes:\n"+IOs.get_text("nomes.txt")+"\n"+
+                   "Letra:\nA!="+((Letter)IOs.get_object("letter.dat")).name);
+       }
+       catch(ClassNotFoundException e){}
+       catch(IOException e){}
     }
 }
